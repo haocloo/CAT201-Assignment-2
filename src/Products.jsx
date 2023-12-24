e9369fe6c6bd34bd1e76ab75f1c71a28f010ec7a
Chi Hao <81066169+haocloo@users.noreply.github.com>
2023-12-25 02:05:51 +0800

    Fix carousel navigation not working when page refresh

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 import Product1 from "./assets/images/Products/Product1.jpeg";
 import Product2 from "./assets/images/Products/Product2.jpeg";
 import Product3 from "./assets/images/Products/Product3.jpeg";
@@ -5,21 +7,32 @@ import Product4 from "./assets/images/Products/Product4.jpeg";
 import Product5 from "./assets/images/Products/Product5.jpeg";
 
 const Products = ({ displayToast }) => {
-  let carouselElement = document.querySelector(".carousel");
+  const carouselElement = useRef(null);
+
+  useEffect(() => {
+    carouselElement.current = document.querySelector(".carousel");
+  }, []);
 
   const scrollCarousel = (targetImageNumber) => {
-    let carouselWidth = carouselElement.clientWidth;
-    console.log(targetImageNumber)
-    // Images are numbered from 1 to 4 so thats why we substract 1
+    if (!carouselElement.current) {
+      console.error('Element with class name "carousel" not found');
+      return;
+    }
+
+    let carouselWidth = carouselElement.current.clientWidth;
+    // Images are numbered from 1 to 4 so that's why we subtract 1
     let targetImage = targetImageNumber - 1;
 
     let targetXPixel = carouselWidth * targetImage + 1;
 
-    carouselElement.scrollTo(targetXPixel, 0);
+    carouselElement.current.scrollTo(targetXPixel, 0);
   };
 
   return (
-    <div id="Products" className="flex flex-col lg:flex-row w -full py-10">
+    <div
+      id="Products"
+      className="flex flex-col lg:flex-row h-[calc(100vh-4rem)] items-center w-full py-10"
+    >
       <div className="flex flex-col gap-3 lg:gap-7 lg:justify-center lg:h-fit p-10 lg:p-14 w-full lg:w-1/2 text-left">
         <h1 className="font-bold text-lg sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl">
           Discover Our Eco-Friendly Products
@@ -50,10 +63,16 @@ const Products = ({ displayToast }) => {
           <div id="slide1" className="carousel-item relative w-full">
             <img src={Product1} className="w-full object-cover object-center" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <button onClick={() => scrollCarousel(5)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(5)}
+                className="btn btn-circle"
+              >
                 ❮
               </button>
-              <button onClick={() => scrollCarousel(2)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(2)}
+                className="btn btn-circle"
+              >
                 ❯
               </button>
             </div>
@@ -61,10 +80,16 @@ const Products = ({ displayToast }) => {
           <div id="slide2" className="carousel-item relative w-full">
             <img src={Product2} className="w-full object-cover object-center" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <button onClick={() => scrollCarousel(1)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(1)}
+                className="btn btn-circle"
+              >
                 ❮
               </button>
-              <button onClick={() => scrollCarousel(3)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(3)}
+                className="btn btn-circle"
+              >
                 ❯
               </button>
             </div>
@@ -72,10 +97,16 @@ const Products = ({ displayToast }) => {
           <div id="slide3" className="carousel-item relative w-full">
             <img src={Product3} className="w-full object-cover object-center" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <button onClick={() => scrollCarousel(2)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(2)}
+                className="btn btn-circle"
+              >
                 ❮
               </button>
-              <button onClick={() => scrollCarousel(4)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(4)}
+                className="btn btn-circle"
+              >
                 ❯
               </button>
             </div>
@@ -83,10 +114,16 @@ const Products = ({ displayToast }) => {
           <div id="slide4" className="carousel-item relative w-full">
             <img src={Product4} className="w-full object-cover object-center" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <button onClick={() => scrollCarousel(3)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(3)}
+                className="btn btn-circle"
+              >
                 ❮
               </button>
-              <button onClick={() => scrollCarousel(5)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(5)}
+                className="btn btn-circle"
+              >
                 ❯
               </button>
             </div>
@@ -94,10 +131,16 @@ const Products = ({ displayToast }) => {
           <div id="slide5" className="carousel-item relative w-full">
             <img src={Product5} className="w-full object-cover object-center" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <button onClick={() => scrollCarousel(4)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(4)}
+                className="btn btn-circle"
+              >
                 ❮
               </button>
-              <button onClick={() => scrollCarousel(1)} className="btn btn-circle">
+              <button
+                onClick={() => scrollCarousel(1)}
+                className="btn btn-circle"
+              >
                 ❯
               </button>
             </div>

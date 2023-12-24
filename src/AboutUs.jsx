91081f872fd8b128ecf8ce93235ef1556e0239af
Chi Hao <81066169+haocloo@users.noreply.github.com>
2023-12-24 18:57:11 +0800

    Edit About Us NPC position

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
       id="AboutUs"
       className="flex flex-col-reverse lg:flex-row-reverse w-full "
     >
-      <div className="flex flex-col justify-center gap-3 lg:gap-7 lg:h-[100vh] p-10 lg:p-14 w-full lg:w-1/2 text-left">
+      <div className="flex flex-col justify-center gap-3 lg:gap-7 lg:h-[100vh] p-10 pb-0 lg:p-14 lg:pb-0 w-full lg:w-1/2 text-left">
         <h1 className="font-bold text-lg sm:text-xl md:text-3xl lg:text-4xl xl:text-5xl">
           About us
         </h1>
